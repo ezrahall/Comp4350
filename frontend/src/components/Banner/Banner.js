25ebb80188a528c963fdbdfebcf6169651300ec0
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-02-19 19:15:52 -0600

    Working on Homepage

---
+++ frontend/src/components/Banner/Banner.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Banner.css"
+import BannerImage from '../images/banner-image2.jpg'
+import {Button} from '@material-ui/core';
+
+function Banner() {
+    return (
+        <div className="banner">
+
+            <div className="banner__info">
+                <h1>
+                    With Safeat
+                </h1>
+                <h5>
+                    Let us help you bring a safer tomorrow
+                </h5>
+                <Button 
+                    variant="outlined">Explore
+                </Button>
+
+            </div>
+            <div className='banner__image'>
+                <img
+                src={BannerImage}
+                alt='' 
+                />
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Banner
--- frontend/src/components/pages/Home/Home.js
+++ frontend/src/components/pages/Home/Home.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import './Home.css'
+import HomeBackground from '../images/Home-Background.jpg'
+import Banner from '../../Banner/Banner'
+import Card from '../../Card/Card'
+import KFC from '../../images/KFC.jpg'
+import Footer from '../../Footer/Footer';
 
 function Home(props) {
     return (
-        <div>
-            <h1>
-                Home
-            </h1>
+        <div className='home'>
+            <div className='home__container'>
+            <img
+                className='home__image'
+                src={HomeBackground}
+                alt = ''
+                    />
+
+            </div>
+            <Banner />
+            <div className='home__cardContainer'>
+
+                <div className='home__card'>
+                <Card title='KFC' image={KFC} rating={4}/>
+                <Card title='KFC' image={KFC} rating={4}/>
+                <Card title='KFC' image={KFC} rating={4}/>
+
+                </div>
+
+
+                <div className='home__card'>
+                <Card title='KFC' image={KFC} rating={4}/>
+                <Card title='KFC' image={KFC} rating={4}/>
+                <Card title='KFC' image={KFC} rating={4}/>
+
+                </div>
+
+                <Footer />
+
+
+
+
+            </div>
         </div>
     );
 }
