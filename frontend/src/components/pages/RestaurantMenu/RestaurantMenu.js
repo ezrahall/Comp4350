e7f38c3e094fa5e8fe0566c6ff5eae17a9041be5
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-02-20 11:49:46 -0600

    Restaurant Menu component and Css file created

---
+++ frontend/src/components/pages/RestaurantMenu/RestaurantMenu.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './RestaurantMenu.css'
+import NavBar from '../../NavBar/NavBar'
+
+function RestaurantMenu() {
+    return (
+        <div className='restaurantMenu'>
+
+            <NavBar />
+
+            <div className='restaurantMenu__image'>
+            </div>
+
+            <div className='restaurantMenu__items'>
+
+            </div>    
+
+
+            
+        </div>
+    )
+}
+
+export default RestaurantMenu
