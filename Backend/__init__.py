f8b989ea5440fb3211f0b7be42a9cd742a26293b
Matt <mathius934@gmail.com>
2021-02-19 22:28:22 -0600

    Added cors policy to enable development locally. Need to disable before final release

--- Backend/__init__.py
+++ Backend/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from gevent import monkey
 import os
@@ -15,6 +16,7 @@ def create_app():
     appl.secret_key = b'"xR\xacBR\xcbx\xc4\xf6\x06\x06y\xcc\x9c\x19'
     appl.config['MAX_CONTENT_LENGTH'] = 32 * 1024 * 1024
 
+    cors = CORS(appl)
     db.init_app(appl)
     login_manager.init_app(appl)
     monkey.patch_all()
