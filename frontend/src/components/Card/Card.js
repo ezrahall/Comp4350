b1ba922855aae6902f16660ede67209847c8ccd2
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-02-20 22:54:26 -0600

    typo Fixed

--- frontend/src/components/Card/Card.js
+++ frontend/src/components/Card/Card.js
@@ -12,7 +12,7 @@ function Card({id,title,image,description,rating,distance}) {
             <h2>{title}</h2>
                 <h4>{description}</h4>
                 <h3>{rating} <img className='card__starImage' src={Star} alt='' /> </h3>
-                <h4>Distace: {distance} KM</h4>
+                <h4>Distance: {distance} KM</h4>
                 
 
             </div>
