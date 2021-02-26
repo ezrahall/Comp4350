c8d82f0d5568f34fa5b6bad214c74a60ddaf1756
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-02-26 15:23:47 -0600

    Banner component refactored

--- frontend/src/components/Banner/Banner.js
+++ frontend/src/components/Banner/Banner.js
@@ -1,26 +1,27 @@
 import React from 'react'
-import "./Banner.css"
 import BannerImage from '../../assets/images/banner-image2.jpg'
 import {Button} from '@material-ui/core';
 
+import styles from "../styles/Banner.module.css"
+
 function Banner() {
     return (
-        <div className="banner">
+        <div className={styles.banner}>
 
-            <div className="banner__info">
+            <div className={styles.banner__info}>
                 <h1>
                     With Safeat
                 </h1>
                 <h5>
                     Let us help you bring a safer tomorrow
                 </h5>
-                <Button
+                <Button 
                     variant="outlined">Explore
-                </Button >
+                </Button>
 
 
             </div>
-            <div className='banner__image'>
+            <div className={styles.banner__image}>
                 <img
                 src={BannerImage}
                 alt='' 
