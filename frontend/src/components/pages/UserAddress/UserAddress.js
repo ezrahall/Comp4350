42c88aad7ee03d3e14c4c6bf8d57cb592dd69c72
Le-DarkOverlord <james_harvey@hotmail.ca>
2021-02-25 20:20:19 -0600

    Added find address page
    Added page that forces user to select from a dropdown of autocompleted addresses to make sure its a valid address

--- frontend/src/components/pages/UserAddress/UserAddress.js
+++ frontend/src/components/pages/UserAddress/UserAddress.js
@@ -4,52 +4,65 @@ import { Button } from '@material-ui/core';
 
 import './UserAddress.css';
 import { AddressContext } from './address';
-import bannerimage from '../../../assets/images/banner-image.jpg'
-
 
 function UserAddress(props) {
     const FIND_RESTAURANTS = "Find Restaurants Nearby";
     const CHOOSE_ADDRESS = "Please choose an address from one of the options";
     const {addresses, token, hasError, respMessage, isLoading, enterAddress} = useContext(AddressContext)
-    const [addrButtonName, setAddrButtonName] = useState(FIND_RESTAURANTS);
-    const [newAddress, setNewAddress] = useState(addresses)
-    const [newToken] = useState(token)
-    const state = {validAddress: false};
-
-    // const {address}  = useContext(UserContext);
-    // const [newAddress, setNewAddress] = useState(address);
+    const [buttonAttr, setButtonAttr] = useState({name: FIND_RESTAURANTS, color: "primary"});
+    const [newAddress, setNewAddress] = useState('')
+    const [newToken, setNewToken] = useState(token)
+    const [validAddress, setValidAddress] = useState(false)
+    const [visible, setVisibility] = useState(false)
+    const [completeAddress, setCompleteAddress] = useState('66 Chancellors Circle, Winnipeg, MB, Canada')
 
     const handleSubmit = (event) => {
-        event.preventDefault();
-        setAddrButtonName(CHOOSE_ADDRESS);
-        // if(!this.state.validAddress)
-        // {
-            
-        // }
-        // const data = {
-        //     address: newAddress
-        // }
+        if(validAddress) {
+            setCompleteAddress(newAddress)
+        }
+        else {
+            event.preventDefault()
+            setButtonAttr({name: CHOOSE_ADDRESS, color: "secondary"})
+        }
     }
 
     const handleChange = (event) => {
         setNewAddress(event.target.value)
-        const data = {
-            address: newAddress,
-            token: newToken
+        setNewToken(token)
+        setValidAddress(false)
+        if(event.target.value != '') {
+            enterAddress(newAddress, newToken)
+            setVisibility(true)
         }
-        enterAddress(newAddress, newToken)
+        else {
+            setVisibility(false)
+        }
+    }
+
+    const handleClick = (event) => {
+        setNewAddress(event.target.innerText)
+        setVisibility(false)
+        setValidAddress(true)
+        setButtonAttr({name: FIND_RESTAURANTS, color: "primary"})
     }
 
     return (
         <div className="enter-address">
             <div className="image-container">
-                <h1 className="title">Safeat A Smarter Way To Eat</h1>
+                <h1 className="title">Safeat: A Smarter Way To Eat</h1>
                 <form onSubmit={handleSubmit} >
                         <div className="address-box">
-                            <input type="text" onChange={handleChange} placeholder="Please Enter Your Address"/>
+                            <input type="text"
+                                   onChange={handleChange} 
+                                   placeholder="Please Enter Your Address"
+                                   value={newAddress}/>
                             <SearchIcon />
                         </div>
-                        <Button className="address-button" type="submit" variant="contained" color="primary">{addrButtonName}</Button>
+                        <div className="address-list">
+                            {visible && addresses.map(address => <li key={address.name} className="address-list-item">
+                            <a onClick={handleClick}>{address.name}</a></li>)}
+                        </div>
+                        <Button className="address-button" type="submit" variant="contained" color={buttonAttr.color}>{buttonAttr.name}</Button>
                 </form>
             </div> 
         </div>
--- frontend/src/components/pages/UserAddress/address.js
+++ frontend/src/components/pages/UserAddress/address.js
@@ -1,66 +1,37 @@
 import React, { createContext, useState } from 'react'
-import nookies, {destroyCookie} from 'nookies'
-import {useHistory, useLocation} from 'react-router-dom'
 import axios from 'axios'
-//import jquery from 'jquery'
 
 export const AddressContext = createContext()
 
 const AddressContextProvider = (props) => {
-    const [addresses, setAddresses] = useState('')
+    const [addresses, setAddresses] = useState([])
     const [token, setToken] = useState('')
     const [hasError, setHasError] = useState(false)
 	const [respMessage, setRespMessage] = useState('')
     const [isLoading, setIsLoading] = useState(false)
-	const location = useLocation()
-	const history = useHistory()
-	const { from } = location.state || { from: { pathname: "/address" } };
 
     const enterAddress = async(currAddress,currToken) => {
-        console.log("Address: ", addresses);
         try {
             setIsLoading(true)
-            const resp = await axios.post(`http://127.0.0.1:5000/Api/User/Autocomplete`, {
+            const resp = await axios.post(`${process.env.REACT_APP_PUBLIC_SERVER_URL}/Api/Search/Autocomplete`, {
                 addr: currAddress,
                 token: currToken
             })
             setIsLoading(false)
-            console.log(resp);
             const statusCode = resp.status
-            console.log(statusCode);
             const data = resp.data
             if (statusCode == 200) {
                 setAddresses(data.completions)
                 setToken(data.token)
-                console.log("Resp headers: ", resp.headers);
                 return true
             }
         } catch(error) { 
             setIsLoading(false)
             setHasError(true)
-            setRespMessage('Error setting address')
-            // if(error.response) {
-            //     switch (error.response.status) {
-            //         case 403: 
-            //             setRespMessage('Username or password is incorrect')
-            //             break
-                        
-            //         case 404: 
-            //             setRespMessage('Error trying to connect to server, please try again later!')
-            //             break
-
-            //         case 500: 
-            //             setRespMessage('Oops well that didnt work. How about you try again. Contact us if the problem persists')
-            //             break
-            //     }
-            // }
-            
+            setRespMessage('Error setting address')     
             return false
         }
     }
-	const setCookies = (cookie) => {
-		nookies.set("sessionCookie", cookie)
-	}
 
 	return (
 		<AddressContext.Provider value={{addresses, token, hasError, respMessage, isLoading, enterAddress}}>
