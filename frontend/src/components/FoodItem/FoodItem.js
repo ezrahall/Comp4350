9391159869f0912ca826acfd7d8a64c9a6f9923f
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-03-16 16:50:31 -0500

    #156 New FoodItem created

--- frontend/src/components/FoodItem/FoodItem.js
+++ frontend/src/components/FoodItem/FoodItem.js
@@ -4,42 +4,36 @@ import {useDispatch} from "react-redux";
 import styles from '../../assets/styles/FoodItem.module.css'
 import {addToBasket as addBasket} from '../../store/actions/cart';
 
-function FoodItem({heading,data}) {
+const FoodItem = (props) => {
     const dispatch = useDispatch();
 
-    const addToBasket = (e) => {
+    const addToBasket = () => {
         dispatch(addBasket({
-            id: e.id,
-            title: e.title,
-            image: e.image,
-            price: e.price,
-            ingridients: e.ingridients
+            id: props.id,
+            title: props.title,
+            image: props.image,
+            price: props.price,
+            ingridients: props.ingridients
         }));
     };
 
     return (
-        <div className={styles.fooditem}>
-            <h1 className={styles.fooditem__heading}>{heading}</h1>
-            <div className={styles.fooditem__wrapper}>
-                {data.map((product) => {
-                    return(
-                        <div className={styles.fooditem__card}>
-                            <div className={styles.fooditem__content}>
-                            <img className={styles.fooditem__image} src={product.img} alt='' />        
-                            <div className={styles.fooditem__info}>
-                                <h2 className={styles.fooditem__title}>{product.title}</h2>
-                                <p className={styles.fooditem__desc}>{product.desc}</p>
-                                <p className={styles.fooditem__price}>${product.price}</p>
-                                <button className={styles.fooditem__button} onClick={(e) => addToBasket(product)}>{product.button}</button>
-                                {/*Needs to be fixed, Subtotal not appearing */}
-                            </div>          
-                            </div>
-                        </div>
-
-                    );
-                })}
-            </div>
+        <div className={styles.fooditem} onClick={(e) => addToBasket()}>
+            
+        <div className={styles.fooditem__card}>
+        <img className={styles.fooditem__image} src={props.image} alt="" />
+            
+        <div className={styles.fooditem__info}>
+            <h2 className={styles.fooditem__title}>{props.title}</h2>
+            <p className={styles.fooditem__ing}>
+            {props.ing} {/* Ingredients Oerflows */}
+            </p>
+            {/*Needs to be fixed, Subtotal not appearing */}
         </div>
+        <p className={styles.fooditem__price}>${props.price}</p>
+
+        </div>
+    </div>
     )
 }
 
