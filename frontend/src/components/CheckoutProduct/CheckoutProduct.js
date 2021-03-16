0aea8c67a02a2a1169574cc8bde60ff0f6e55733
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-03-16 18:39:28 -0500

    #157 New UI Added

--- frontend/src/components/CheckoutProduct/CheckoutProduct.js
+++ frontend/src/components/CheckoutProduct/CheckoutProduct.js
@@ -1,29 +1,43 @@
-import React from 'react'
-import {useDispatch} from "react-redux";
+import React from "react";
+import { useDispatch } from "react-redux";
 
-import styles from '../../assets/styles/CheckoutProduct.module.css'
-import {removeFromBasket as removeBasket} from '../../store/actions/cart';
+import styles from "../../assets/styles/CheckoutProduct.module.css";
+import { removeFromBasket as removeBasket } from "../../store/actions/cart";
+import CancelIcon from "@material-ui/icons/Cancel";
 
-function CheckoutProduct({id,image,title,price}) {
-    const dispatch = useDispatch()
+const CheckoutProduct = (props) => {
+  const dispatch = useDispatch();
 
-    const removeFromBasket = () => {
-        dispatch(removeBasket(id));
-    }
+  const removeFromBasket = () => {
+    dispatch(removeBasket(props.id));
+  };
 
-    return (
-        <div className={styles.checkoutProduct}>
-            <img className={styles.checkoutProduct__image} src={image} alt={title}/>
-            <div className={styles.checkoutProduct__info}>
-                <p className={styles.chekcoutProduct__title}>{title}</p>
-                <p className={styles.checkoutProduct__price}>
-                    <small>$</small>
-                    <strong>{price}</strong>
-                </p>
-                <button onClick={removeFromBasket}>Remove</button>
+  return (
+    <div class={styles.container}>
+      <div className={styles.checkoutProduct}>
+        <div className={styles.checkoutProduct__container}>
+          <img
+            className={styles.checkoutProduct__image}
+            src={props.image}
+            alt={props.title}
+          />
+          <div className={styles.checkoutProduct__info}>
+            <div className={styles.chekcoutProduct__title}>{props.title}</div>
+            <div className={styles.checkoutProduct__price}>
+              <small>$</small>
+              <strong>{props.price}</strong>
             </div>
+          </div>
         </div>
-    )
-}
+        <div
+          className={styles.checkoutProduct__remove}
+          onClick={removeFromBasket}
+        >
+          <CancelIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default CheckoutProduct
+export default CheckoutProduct;
