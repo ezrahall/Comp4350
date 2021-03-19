23c6294ec75f6857ef4474da6592fbe189895b64
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-03-19 15:45:59 -0500

    #156 Title Added to Restaurant Menu

--- frontend/src/components/pages/RestaurantMenu/RestaurantMenu.js
+++ frontend/src/components/pages/RestaurantMenu/RestaurantMenu.js
@@ -1,26 +1,67 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-import styles from '../../../assets/styles/pages/RestaurantMenu.module.css'
-import Hero from '../../Hero/Hero'
-import FoodItem from '../../FoodItem/FoodItem'
-import FeaturedItem from '../../FoodItem/FeaturedItem/FeaturedItem'
-import Footer from '../../Footer/Footer'
-import {foodData} from '../../FoodItem/data'
+import styles from "../../../assets/styles/pages/RestaurantMenu.module.css";
+import Hero from "../../Hero/Hero";
+import FoodItem from "../../FoodItem/FoodItem";
+import FeaturedItem from "../../FoodItem/FeaturedItem/FeaturedItem";
+import Footer from "../../Footer/Footer";
+import { foodData } from "../../FoodItem/data";
 import NavBar from "../../NavBar/NavBar";
-import KFC from "../../../assets/images/KFC.jpg"
+import KFC from "../../../assets/images/KFC.jpg";
 
 function RestaurantMenu() {
-    return (
-        <div className={styles.restaurantMenu}>
-            <NavBar/>
-            <Hero />
-            <FoodItem image={KFC} id="1" title="Chicken" ing="ashdkajshdaljsdhasjdgakhjsdgashdgaslhjdagsda" price="13.33" />
-            <FoodItem image={KFC} id="2" title="Chicken" ing="ashdkajshdaljsdhasjdgakhjsdgashdgaslhjdagsda" price="13.33" />
-            <FoodItem image={KFC} id="3" title="Chicken" ing="ashdkajshdaljsdhasjdgakhjsdgashdgaslhjdagsda" price="13.33" />
-            <FoodItem image={KFC} id="4" title="Chicken" ing="ashdkajshdaljsdhasjdgakhjsdgashdgaslhjdagsda" price="13.33" />
-            <Footer />
-        </div>
-    )
+
+  return (
+    <div className={styles.restaurantMenu}>
+      <NavBar />
+      <Hero />
+      <br />
+      <br />
+      <div className={styles.restaurantMenu__titleContainer}>
+        <div className={styles.restaurantMenu__title}>Menu:</div>
+
+        <div className={styles.restaurantMenu__subtitle}>
+          Click below to select items
+        </div> 
+
+      </div>
+      <br />
+
+      <FoodItem
+        image={KFC}
+        id="1"
+        title="Chicken"
+        ing="ashdkajshdaljsdhasjdgakhjsdgashdgaslhjdagsda"
+        price="13.33"
+      />
+      <FoodItem
+        image={KFC}
+        id="2"
+        title="Chicken"
+        ing="ashdkajshdaljsdhasjdgakhjsdgashdgaslhjdagsda"
+        price="13.33"
+      />
+      <FoodItem
+        image={KFC}
+        id="3"
+        title="Chicken"
+        ing="ashdkajshdaljsdhasjdgakhjsdgashdgaslhjdagsda"
+        price="13.33"
+      />
+      <FoodItem
+        image={KFC}
+        id="4"
+        title="Chicken"
+        ing="ashdkajshdaljsdhasjdgakhjsdgashdgaslhjdagsda"
+        price="13.33"
+      />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Footer />
+    </div>
+  );
 }
 
-export default RestaurantMenu
+export default RestaurantMenu;
