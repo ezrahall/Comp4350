b7fbb41f2c26777208d62718d5e34fc14d25b607
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-03-19 13:25:22 -0500

    #157 Subtotal Re-adjusted to new basket

--- frontend/src/components/Subtotal/Subtotal.js
+++ frontend/src/components/Subtotal/Subtotal.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useEffect,useState} from 'react';
 import CurrencyFormat from 'react-currency-format';
 import {useSelector} from "react-redux";
 import { useHistory } from 'react-router-dom';
@@ -8,6 +8,18 @@ import styles from '../../assets/styles/Subtotal.module.css';
 const Subtotal = () => {
     const basket = useSelector(state => state.cart.basket)
 
+
+    const [basketCount, setbasketCount] = useState(0);
+
+    useEffect(() => {
+        let count = 0;
+        basket.forEach((item) => {
+          count += item.qty;
+        });
+    
+        setbasketCount(count);
+      }, [basket, basketCount]);
+
     const history = useHistory();
 
     return (
@@ -16,7 +28,7 @@ const Subtotal = () => {
                 renderText={(value) => (
                     <>
                         <p>
-                            Subtotal ({basket.length} items):
+                            Subtotal ({basketCount} items):
                             <strong>{value}</strong>
                         </p>
                         <small className={styles.subtotal__gift}>
@@ -25,7 +37,7 @@ const Subtotal = () => {
                     </>
                 )}
                 decimalScale={2}
-                value={basket.reduce((amount, item) => parseFloat(item.price) + amount, 0)}
+                value={basket.reduce((amount, item) => (parseFloat(item.price) * item.qty )+ amount, 0)}
                 displayType={"text"}
                 thousandSeparator={true}
                 prefix={"$"}
