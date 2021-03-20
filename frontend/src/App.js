5dfdbec6756c8f6bea53fcf0ed577d0abc26f08f
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-03-20 14:17:39 -0500

    #146 Stripe Added to Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import {useEffect} from "react";
 import {Switch, Route, withRouter} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
+import {Elements} from '@stripe/react-stripe-js';
+import {loadStripe} from '@stripe/stripe-js';
 
 
 import './App.css';
@@ -15,6 +17,8 @@ import UserAddress from './components/pages/UserAddress/UserAddress';
 import RestaurantDetails from './components/pages/RestaurantDetails/RestaurantDetails';
 import CovidReport from "./components/pages/CovidReport/CovidReport";
 import {authSuccess} from './store/actions/user';
+
+const stripePromise = loadStripe('pk_test_51IWvOsCXMychAZM499t1cB9kFug8Z5AvB9FpXhSnpsCcOGCXz1OervvAlKPzbg5VzYz2Ro5UGDxtYQHk2A0p1zw0002D2xp1OP');
 
 const LoginContainer = () => {
 
@@ -34,7 +38,9 @@ const DefaultContainer = () => {
           <Route path="/account"component={Account}/>
           <Route path="/dashboard" component={RestaurantDetails}/>
           <Route path="/checkout"component={Checkout}/>
+          <Elements stripe={stripePromise}>
           <Route path="/payment"component={Payment}/>
+          </Elements>
           <Route path="/report"component={CovidReport}/>
       </div>
     </div>
