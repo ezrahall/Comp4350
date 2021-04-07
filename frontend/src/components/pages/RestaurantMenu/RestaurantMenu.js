e0125c78b2a75c98335ce12a0aae379d2e56ed3f
Tahmidul Hasnain Zidaan <thzidaan@gmail.com>
2021-04-07 14:58:05 -0500

    #254 New Images Incorporated

--- frontend/src/components/pages/RestaurantMenu/RestaurantMenu.js
+++ frontend/src/components/pages/RestaurantMenu/RestaurantMenu.js
@@ -1,14 +1,19 @@
 import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux';
 
 import styles from '../../../assets/styles/pages/RestaurantMenu.module.css'
 import Hero from '../../Hero/Hero'
 import FoodItem from '../../FoodItem/FoodItem'
 import NavBar from '../../NavBar/NavBar';
 import KFC from '../../../assets/images/KFC.jpg';
+import ClickImage from '../../../assets/images/menuClick.png'
+import ItemImage from '../../../assets/images/menuItemImage.jpg'
 import {getRestaurantMenu} from '../../../services/restaurants/restaurantsService';
 
 function RestaurantMenu() {
     const [menuItems, setMenuItems] = useState([])
+    const restaurant = useSelector(state => state.restaurant.restaurant)
+    const basket = useSelector(state => state.cart.basket)
 
 
 
@@ -20,19 +25,34 @@ function RestaurantMenu() {
     },[])
 
     return (
-        <div className={styles.restaurantMenu}>
+        <div className={styles.menu}>
             <NavBar />
             <br />
             <Hero />
             <br />
-            <div className={styles.restaurantMenu__titleContainer}>
-                <div className={styles.restaurantMenu__title}>Menu:</div>
+            <div className={styles.menu__titleContainer}>
+                
 
-                <div className={styles.restaurantMenu__subtitle}>
-                    Click below to select items
+                <div className={styles.menu__subtitle}>
+                    Delivery Time: {restaurant.time}
                 </div>
 
             </div>
+            { basket?.length === 0 ?             
+                <div className={styles.menu__clickContainer} >
+                <img className={styles.menu__menuImage}  src={ClickImage} alt ='' />
+                </div>
+                :
+                <div></div>
+                
+            }
+            
+            <div className={styles.menu__itemImageContainer} >
+                <img className={styles.menu__menuItem}  src={ItemImage} alt ='' />
+                
+            </div>
+            <br />
+ 
             {menuItems.map((item) => <FoodItem
                 key={item.id}
                 image={KFC}
